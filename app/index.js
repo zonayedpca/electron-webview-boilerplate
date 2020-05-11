65821e40cc5aa16352b77f8cd56cf0481c56dc9d
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-05-12 01:54:38 +0600

    client from env variable

--- app/index.js
+++ app/index.js
@@ -3,16 +3,31 @@ const { app } = require("electron");
 
 const MyBrowserWindow = require("./module/MyBrowserWindow");
 
+require("dotenv").config();
+
+const { CLIENT, HEIGHT, WIDTH } = process.env;
+
+const config = {
+	client: CLIENT,
+	height: HEIGHT || 600,
+	width: WIDTH || 480,
+};
+
 let mainWindow;
 
 app.on("ready", () => {
+	const { client, height, width } = config;
 	mainWindow = new MyBrowserWindow({
-		height: 600,
-		width: 480,
+		height,
+		width,
 	});
-	const filePath = path.join(__dirname, "..", "client");
-	const file = `${filePath}/index.html`;
-	mainWindow.loadFile(file);
+	if (client) {
+		mainWindow.loadURL(client);
+	} else {
+		const filePath = path.join(__dirname, "..", "client");
+		const file = `${filePath}/index.html`;
+		mainWindow.loadFile(file);
+	}
 });
 
 app.on("window-all-closed", () => {
